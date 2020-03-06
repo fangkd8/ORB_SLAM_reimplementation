bfa81129cd3448785eb0ec4c0cc1341cee459023
fangkd <fangkd@umich.edu>
2020-03-06 16:11:24 -0500

    add mappoints update

--- VisualOdometry/include/VisualOdometry/mappoint.h
+++ VisualOdometry/include/VisualOdometry/mappoint.h
@@ -1,6 +1,7 @@
 #ifndef MAPPOINT_H
 #define MAPPOINT_H
 
+#include "VisualOdometry/frame.h"
 #include "VisualOdometry/common_include.h"
 namespace VisualOdometry
 {
@@ -11,23 +12,44 @@ public:
   typedef std::shared_ptr<MapPoint> Ptr;
   unsigned long id_;
   // position in world frame.
-  Eigen::Vector3d pos;
+  Eigen::Vector3d pos_;
   // Normal of viewing direction.
   Eigen::Vector3d norm_;
   // ORB descriptor
-  cv::Mat descriptor;
+  cv::Mat descriptor_;
   // times of being observed by feature matching.
   int observed_;
   // times of being an inlier for pose estimation.
   int correct_;
+
+  static long factory_id_;
+  bool good_; // whether it is a good point.
+
+  std::list<Frame*> observed_frames_;
+  // keyframes that can observe this mappoint
+  int matched_times_; // being one of inliers in PnP
+  int visible_times_; // being visible in current frames.
 
 public:
   MapPoint();
 
   MapPoint(long id, Eigen::Vector3d position, Eigen::Vector3d normal);
 
+  MapPoint(unsigned long id, const Eigen::Vector3d& position,
+           const Eigen::Vector3d& norm, Frame* frame = nullptr, 
+           const cv::Mat& descriptor = cv::Mat());
+
+  inline cv::Point3f getPositionCV() const {
+    return cv::Point3f( pos_(0,0), pos_(1,0), pos_(2,0) );
+  }
+
   // still not clear of this function, same as frame.h
   static MapPoint::Ptr createMapPoint();
+  static MapPoint::Ptr createMapPoint(
+    const Eigen::Vector3d& position,
+    const Eigen::Vector3d& norm,
+    const cv::Mat& descriptor, 
+    Frame* frame = nullptr);
 };
 
 }
--- VisualOdometry/src/mappoint.cpp
+++ VisualOdometry/src/mappoint.cpp
@@ -3,17 +3,35 @@
 namespace VisualOdometry
 {
 MapPoint::MapPoint():
-  id_(-1), pos(Eigen::Vector3d(0, 0, 0)), norm_(Eigen::Vector3d(0, 0, 0)), 
+  id_(-1), pos_(Eigen::Vector3d(0, 0, 0)), norm_(Eigen::Vector3d(0, 0, 0)), 
   observed_(0), correct_(0) {};
 
 MapPoint::MapPoint(long id, Eigen::Vector3d position, Eigen::Vector3d normal):
-  id_(id), pos(position), norm_(normal), observed_(0), correct_(0) {};
+  id_(id), pos_(position), norm_(normal), observed_(0), correct_(0) {};
 
-MapPoint::Ptr createMapPoint(){
+MapPoint::MapPoint(unsigned long id, const Eigen::Vector3d& position,
+                   const Eigen::Vector3d& norm, Frame* frame, 
+                   const cv::Mat& descriptor):
+  id_(id), pos_ (position), norm_ (norm), good_(true), visible_times_(1), 
+  matched_times_(1), descriptor_(descriptor){
+    observed_frames_.push_back(frame);
+  }
+
+MapPoint::Ptr MapPoint::createMapPoint(){
   static long factory_id = 0;
   return MapPoint::Ptr(new MapPoint(factory_id++, 
                            Eigen::Vector3d(0, 0, 0), 
                            Eigen::Vector3d(0, 0, 0)));
+}
+
+MapPoint::Ptr MapPoint::createMapPoint(
+    const Eigen::Vector3d& position,
+    const Eigen::Vector3d& norm,
+    const cv::Mat& descriptor, 
+    Frame* frame){
+  static long factory_id = 0;
+  return MapPoint::Ptr(new MapPoint(factory_id++, 
+                           position, norm, frame, descriptor));
 }
 
 }
